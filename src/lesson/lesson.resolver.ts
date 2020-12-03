d663d1c6bae6bc3be4eb0198cc7b4abf2f558432
John Huynh <jhnhnyh9@gmail.com>
2020-12-02 21:55:12 -0800

    add logic to link lessons to students

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql/dist';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { CreateLessonInput } from './lesson.input';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
@@ -21,5 +22,14 @@ export class LessonResolver {
   @Query((returns) => [LessonType])
   lessons() {
     return this.lessonService.getLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
   }
 }
